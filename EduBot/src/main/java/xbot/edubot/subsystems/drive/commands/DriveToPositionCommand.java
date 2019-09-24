73fafe6ceebab6439311ea2ffc88ab4937164b6a
lil206chris <big206chris@gmail.com>
2019-09-24 00:41:43 -0700

    got it to stop near but not at 0

--- EduBot/src/main/java/xbot/edubot/subsystems/drive/commands/DriveToPositionCommand.java
+++ EduBot/src/main/java/xbot/edubot/subsystems/drive/commands/DriveToPositionCommand.java
@@ -10,7 +10,7 @@ public class DriveToPositionCommand extends BaseCommand {
 
     DriveSubsystem drive;
     PoseSubsystem pose;
-    
+    double goal;
     @Inject
     public DriveToPositionCommand(DriveSubsystem driveSubsystem, PoseSubsystem pose) {
         this.drive = driveSubsystem;
@@ -18,6 +18,7 @@ public class DriveToPositionCommand extends BaseCommand {
     }
     
     public void setTargetPosition(double position) {
+        goal= position;
         // This method will be called by the test, and will give you a goal distance.
         // You'll need to remember this target position and use it in your calculations.
     }
@@ -33,14 +34,30 @@ public class DriveToPositionCommand extends BaseCommand {
         // - Gets the robot to move to the target position 
         // - Hint: use pose.getPosition() to find out where you are
         // - Gets the robot stop (or at least be moving really really slowly) at the target position
-        
         // How you do this is up to you. If you get stuck, ask a mentor or student for some hints!
+        if(isFinished())
+        {
+            drive.tankDrive(-1, -1);
+        }
+        else
+        {
+            drive.tankDrive(1, 1);
+            if((goal - 2) < pose.getPosition() && goal > pose.getPosition())
+            {
+                drive.tankDrive(.01, .01);
+            }
+        }
     }
     
     @Override
     public boolean isFinished() {
         // Modify this to return true once you have met your goal, 
         // and you're moving fairly slowly (ideally stopped)
+        if((goal + .1)>pose.getPosition() && (goal - .3) < pose.getPosition())
+        {
+            drive.tankDrive(-1, -1);
+            return true;
+        }
         return false;
     }
 
